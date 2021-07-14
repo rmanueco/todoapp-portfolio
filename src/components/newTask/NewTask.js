434fb4188177a2ab72221263d38e4401b528efa6
rmanueco <manueco.rodrigo@gmail.com>
2021-07-14 20:10:43 +0200

    Add onKeyPress Event

--- src/components/newTask/NewTask.js
+++ src/components/newTask/NewTask.js
@@ -15,6 +15,7 @@ function NewTask({ tasks, setTasks }) {
                     <i className='fas fa-arrow-down'></i>
                 </div>
                 <input
+                    id='focus'
                     value={newTask}
                     onChange={(e) => {
                         setNewTask(e.target.value);
@@ -22,15 +23,45 @@ function NewTask({ tasks, setTasks }) {
                     maxLength='20'
                     type='text'
                     placeholder='Enter a new task'
+                    autoFocus
+                    onKeyPress={(e) => {
+                        if (e.key === 'Enter') {
+                            document.getElementById('text-area').focus();
+                        }
+                    }}
                 />
                 {showErrorInput && <p>You must enter at least 5 character</p>}
                 <textarea
+                    id='text-area'
                     value={newDescription}
                     onChange={(e) => setNewDescription(e.target.value)}
                     rows='3'
                     maxLength='87'
                     type='text'
                     placeholder='Description'
+                    onKeyPress={(e) => {
+                        if (e.key === 'Enter') {
+                            if (newTask.length < 5) {
+                                setShowErrorInput(true);
+                            } else if (newDescription.length < 10) {
+                                setShowErrorInput(false);
+                                setshowErrorDescription(true);
+                            } else {
+                                setTasks(
+                                    tasks.concat({
+                                        id: tasks.length + 1,
+                                        task: newTask,
+                                        description: newDescription,
+                                    })
+                                );
+                                setNewTask('');
+                                setNewDescription('');
+                                setShowErrorInput(false);
+                                setshowErrorDescription(false);
+                                document.getElementById('focus').focus();
+                            }
+                        }
+                    }}
                 />
                 {showErrorDescription && (
                     <p>You must enter at least 10 character</p>
@@ -54,6 +85,7 @@ function NewTask({ tasks, setTasks }) {
                             setNewDescription('');
                             setShowErrorInput(false);
                             setshowErrorDescription(false);
+                            document.getElementById('focus').focus();
                         }
                     }}
                 >
